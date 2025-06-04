6a070d9f52e65671bc8dd37e9882c2afb4b25b62
burak-ezer <burakezerrr@gmail.com>
2025-06-04 13:09:51 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,8 @@
 const getUserNames = (users) => {
+    let dizi = [];
+    for(const user of users) {
+        dizi.push(user.name);
+    }
+    console.log(dizi);
   return users.map((user) => user.name);
-  console.log(user.name);
 };
